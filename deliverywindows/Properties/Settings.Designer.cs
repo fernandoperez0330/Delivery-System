c1d8f0acef4623ed04cdbd317fd9fd156bec9ca0
adderly gonzalez <adderlygonzalez@gmail.com>
2011-11-29 02:14:52 -0300

    SupplierManage + New DBFile

--- deliverywindows/Properties/Settings.Designer.cs
+++ deliverywindows/Properties/Settings.Designer.cs
@@ -22,5 +22,15 @@ namespace deliverywindows.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=deliverydata;Integrated Security=True")]
+        public string deliverydataConnectionString {
+            get {
+                return ((string)(this["deliverydataConnectionString"]));
+            }
+        }
     }
 }
